26a59131f4cd271a8e9a14c05b7c4e1208aa28a9
AntonFrisch <131719653+AntonFrisch@users.noreply.github.com>
2023-11-20 14:22:58 +0100

    Anadir Ejercicio 4

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Bienvenido a mi página web exprés' });
+});
+
+module.exports = router;
